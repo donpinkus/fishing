554245d26cbf3a6d5ccc2f4891d1946219cd00b2
Don Pinkus <don.pinkus@gmail.com>
2020-11-29 22:18:54 -0800

    Max depth text updates & cleaned up folders

---
+++ Assets/scripts/MaxDepthText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MaxDepthText : MonoBehaviour
+{
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<Text>().text = Player.GetComponent<Player>().maxDepth + " m";
+    }
+}
